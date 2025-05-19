1a566888a20c0b504220ec6eb1fd03fc72aba224
AminaAzzmouri <azzmouriamina@gmail.com>
2025-05-18 23:52:48 -0700

    Fix syntax error

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,7 @@
 const args = process.argv.slice(2);
 
-if (args.lenght === 0) {
-	console.log("No argument");
-] else if (args.length === 1) {
-	console.log("Argument found");
-} else {
-	console.log("Arguments found");
-}
+if (args.lenght === 0) 
+	{ console.log("No argument");}
+else if (args.length === 1) 
+	{ console.log("Argument found");}
+else { console.log("Arguments found");}
